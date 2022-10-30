7019f08c457d76886fafb2f266f7d2e3b1ad0c87
decapacitator <anushkabishnoi9@gmail.com>
2022-10-30 11:28:16 +0530

    second commit

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -7,7 +7,14 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
-enum eDirection {STOP=0 , LEFT , RIGHT , UP , DOWN};
+enum eDirection
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
 eDirection dir;
 int tailX[100], tailY[100];
 int nTail;
@@ -25,70 +32,70 @@ void Setup()
 void Draw()
 {
     system("cls");
-    //Starting border (top wall)
-    for(int i = 0; i < width+2; i++)
-        cout<<"#";
-    cout<<endl;
-    //Printing the map
-    for(int i = 0; i < height; i++)
+    // Starting border (top wall)
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    // Printing the map
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width+2; j++)
+        for (int j = 0; j < width + 2; j++)
         {
-            if(j==0)
-                cout<<"#";
-            //snake head
-            if( i == y && j == x)
-                cout<<"O";
-            //fruit
-            else if(i == fruitY && j == fruitX)
+            if (j == 0)
+                cout << "#";
+            // snake head
+            if (i == y && j == x)
+                cout << "O";
+            // fruit
+            else if (i == fruitY && j == fruitX)
                 cout << "F";
             else
             {
                 bool print = false;
                 for (int k = 0; k < nTail; k++)
                 {
-                    if(tailX[k] == j && tailY[k] == i)
+                    if (tailX[k] == j && tailY[k] == i)
                     {
-                        cout<<"o";
+                        cout << "o";
                         print = true;
-                    }  
+                    }
                 }
-                if(!print)
-                    cout<<" ";   
+                if (!print)
+                    cout << " ";
             }
-            if(j==width-1)
-                cout<<"#";
+            if (j == width - 1)
+                cout << "#";
         }
-        cout<<endl;
+        cout << endl;
     }
-    //Lower border (bottom wall)
-    for( int i = 0; i < width+2 ; i++)
-        cout<<"#";
-    cout<<endl;
-    cout<< "Score: "<< score << endl;
+    // Lower border (bottom wall)
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score: " << score << endl;
 }
 void Input()
 {
-    //if any key pressed
-    if( _kbhit())
+    // if any key pressed
+    if (_kbhit())
     {
-        //take input
+        // take input
         switch (_getch())
         {
         case 'a':
-            dir = LEFT ;
+            dir = LEFT;
             break;
         case 'd':
-            dir = RIGHT ;
+            dir = RIGHT;
             break;
         case 'w':
-            dir = UP ;
+            dir = UP;
             break;
         case 's':
-            dir = DOWN ;
+            dir = DOWN;
             break;
         case 'x':
-             gameOver = true ;
+            gameOver = true;
         default:
             break;
         }
@@ -101,7 +108,7 @@ void Logic()
     int prev2X, prev2Y;
     tailX[0] = x;
     tailY[0] = y;
-    for(int i = 1; i< nTail; i++)
+    for (int i = 1; i < nTail; i++)
     {
         prev2X = tailX[i];
         prev2Y = tailY[i];
@@ -127,39 +134,45 @@ void Logic()
     default:
         break;
     }
-    //CAN ENABLE EITHER 1 OR 2
-//1  //if border touched or -ve values
-     //game over
+    // CAN ENABLE EITHER 1 OR 2
+    // 1  //if border touched or -ve values
+    // game over
     /*
     if( x > width || x < 0 || y > height || y < 0 )
         gameOver = true ;
         */
-//2  //if border crossed, would enter
-     //box from other side
-    if(x >= width) x=0; else if(x < 0) x = width-1;
-    if(y >= height) y=0; else if(y < 0) y = height-1;
-    //increase tail length 
-    //everytime fruit coordinates matched
-    for(int i = 0; i< nTail; i++)
-    if( tailX[i]==x && tailY[i]==y )
-        gameOver = true;
-    if( x==fruitX && y==fruitY)
+    // 2  //if border crossed, would enter
+    // box from other side
+    if (x >= width)
+        x = 0;
+    else if (x < 0)
+        x = width - 1;
+    if (y >= height)
+        y = 0;
+    else if (y < 0)
+        y = height - 1;
+    // increase tail length
+    // everytime fruit coordinates matched
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    if (x == fruitX && y == fruitY)
     {
         score += 10;
-        fruitX = rand() % width ;
-        fruitY = rand() % height ;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
         nTail++;
     }
 }
 int main()
 {
     Setup();
-    while(!gameOver)
+    while (!gameOver)
     {
         Draw();
         Input();
         Logic();
-        Sleep(40); //sleep (windows.h library function)
+        Sleep(40); // sleep (windows.h library function)
     }
     return 0;
 }
